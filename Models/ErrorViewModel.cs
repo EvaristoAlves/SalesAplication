09fe8fbf60512d5a64d9b4ab704b896abb1b654d
Evaristo Alves <evaristocarvalho34@gmail.com>
2021-11-12 17:55:35 -0300

    criando aplicacao

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SalesAplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
